5cf639561c5b10bcb06301ea8c692393ea1c7696
triagung128 <triagung128@gmail.com>
2024-08-01 06:28:32 +0700

    fix: set width footer logo

--- src/components/Footer/FooterLogo.jsx
+++ src/components/Footer/FooterLogo.jsx
@@ -1,9 +1,9 @@
 import Logo from "/logo_bg.png";
 
 const FooterLogo = () => (
-  <div className="w-[294px] shrink-0">
+  <div className="shrink-0">
     <img src={Logo} alt="Logo Text" className="mb-3.5" />
-    <p className="text-base font-semibold text-white">
+    <p className="w-[294px] text-base font-semibold text-white">
       Comfortable and Quiet Worship, Our Service No. 1 in Indonesia
     </p>
   </div>
